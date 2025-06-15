3b4ac08353402d743e1ab6d81fb1d2051c8ddc79
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-06-15 13:43:53 +0200

    📝 rename package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Avios",
+    name: "AviosKit",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
