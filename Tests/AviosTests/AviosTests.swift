b690f8b7cad98bd630cee369c8a012e9f8522694
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-06-15 10:49:00 +0200

    🧪 test get method

--- Tests/AviosTests/AviosTests.swift
+++ Tests/AviosTests/AviosTests.swift
@@ -1,6 +1,22 @@
 import Testing
 @testable import Avios
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+fileprivate struct Post: Codable, Hashable {
+    var userId: Int
+    var id: Int
+    var title: String
+    var body: String
+}
+
+@Test func getMethod() async throws {
+    let (data, response) = try await Avios.shared.get("https://jsonplaceholder.typicode.com/posts", headers: nil)
+    
+    // Response have to be fine here
+    #expect(response.isOk())
+    
+    // Trying to decode posts here
+    let posts = try data.decode(into: [Post].self)
+    
+    // Array should be non-empty
+    #expect(posts.count > 0)
 }
