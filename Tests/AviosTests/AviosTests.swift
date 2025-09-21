b44eac13e00ee0dd71c06fd8a551c41d35e53595
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-09-21 15:32:59 +0200

    🧪 update tests

--- Tests/AviosTests/AviosTests.swift
+++ Tests/AviosTests/AviosTests.swift
@@ -33,68 +33,70 @@ fileprivate func expectThatOk(_ route: String = "posts", method: HttpMethod, sta
     #expect(res.httpResponse?.statusCode == statusCode)
 }
 
-@Test func getMethod() async throws {
-    let (data, response) = try await Avios.shared.get(typicodeUrl("posts"), headers: nil)
-    
-    // Response have to be fine here
-    #expect(response.isOk())
-    
-    // Trying to decode posts here
-    let posts = try data.decode(into: [Post].self)
-    
-    // Array should be non-empty
-    #expect(posts.count > 0)
-}
-
-@Test func getSinglePost() async throws {
-    let (data, response) = try await Avios.shared.get(typicodeUrl("posts/2"), headers: nil)
-    
-    // Response have to be fine here
-    #expect(response.isOk())
-    
-    // Trying to decode posts here
-    _ = try data.decode(into: Post.self)
-    
-    // Code below should fail
-    #expect(throws: Error.self) {
-        _ = try data.decode(into: NonPost.self)
+struct MainTests {
+    @Test func getMethod() async throws {
+        let (data, response) = try await Avios.shared.get(typicodeUrl("posts"))
+        
+        // Response have to be fine here
+        #expect(response.isOk())
+        
+        // Trying to decode posts here
+        let posts = try data.decode(into: [Post].self)
+        
+        // Array should be non-empty
+        #expect(posts.count > 0)
     }
-}
 
-@Test func postMethod() async throws {
-    try await expectThatOk("posts", method: .post, statusCode: 201)
-}
+    @Test func getSinglePost() async throws {
+        let (data, response) = try await Avios.shared.get(typicodeUrl("posts/2"))
+        
+        // Response have to be fine here
+        #expect(response.isOk())
+        
+        // Trying to decode posts here
+        _ = try data.decode(into: Post.self)
+        
+        // Code below should fail
+        #expect(throws: Error.self) {
+            _ = try data.decode(into: NonPost.self)
+        }
+    }
 
-@Test func putMethod() async throws {
-    try await expectThatOk("posts/1", method: .put)
-}
+    @Test func postMethod() async throws {
+        try await expectThatOk("posts", method: .post, statusCode: 201)
+    }
 
-@Test func patchMethod() async throws {
-    try await expectThatOk("posts/1", method: .patch)
-}
+    @Test func putMethod() async throws {
+        try await expectThatOk("posts/1", method: .put)
+    }
 
-@Test func deleteMethod() async throws {
-    try await expectThatOk("posts/1", method: .delete)
-}
+    @Test func patchMethod() async throws {
+        try await expectThatOk("posts/1", method: .patch)
+    }
 
-@Test func handleStatusCodes() async throws {
-    let (_, res) = try await Avios.shared.custom(typicodeUrl("this-path-does-not-exist"), method: .patch)
-    #expect(res.httpResponse?.statusCode == 404)
-}
+    @Test func deleteMethod() async throws {
+        try await expectThatOk("posts/1", method: .delete)
+    }
 
-@Test func withBody() async throws {
-    let body = PostDto(title: "foo", body: "bar", usedId: 1)
-    let (data, res) = try await Avios.shared.post(typicodeUrl("posts"), body: body, headers: [
-        "Content-type": "application/json; charset=UTF-8"
-    ])
-    
-    // Check status
-    #expect(res.isOk())
-    #expect(res.httpResponse?.statusCode == 201)
-    
-    // Decoding the result
-    let result = try data.decode(into: PostDto.self)
-    
-    #expect(body.body == result.body)
-    #expect(body.title == result.title)
+    @Test func handleStatusCodes() async throws {
+        let (_, res) = try await Avios.shared.custom(typicodeUrl("this-path-does-not-exist"), method: .patch)
+        #expect(res.httpResponse?.statusCode == 404)
+    }
+
+    @Test func withBody() async throws {
+        let body = PostDto(title: "foo", body: "bar", usedId: 1)
+        let (data, res) = try await Avios.shared.post(typicodeUrl("posts"), body: body, headers: [
+            "Content-type": "application/json; charset=UTF-8"
+        ])
+        
+        // Check status
+        #expect(res.isOk())
+        #expect(res.httpResponse?.statusCode == 201)
+        
+        // Decoding the result
+        let result = try data.decode(into: PostDto.self)
+        
+        #expect(body.body == result.body)
+        #expect(body.title == result.title)
+    }
 }
